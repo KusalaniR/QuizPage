5a007becd2493511228b8d8d54ef1dbb4cc57522
KusalaniR <120073100+KusalaniR@users.noreply.github.com>
2023-09-14 07:39:00 +0530

    Add files via upload

---
+++ quizT.js
@@ -0,0 +1,98 @@
+let questions = [
+    {
+        numb: 1,
+        question: "How high is Pidurangala Rock?",
+        answer: "200 meters",
+        options: [
+          "200 meters",
+          "250 meters",
+          "300 meters",
+          "600 meters"
+        ],
+        
+      },
+        {
+            numb: 2,
+            question: "Which province is Ritigala?",
+            answer: " North Central Province ",
+            options: [
+              " Southern Province ",
+              " North Central Province ",
+              " Western Province ",
+              " Central Province "
+
+            ]
+          },
+            {
+            numb: 3,
+            question: "What is the highest peak of the Knuckles mountain range? ",
+            answer: "Gombaniya",
+            options: [
+              "Gombaniya",
+              "Five Peaks",
+              "Nawanagala",
+              "Velangala"
+            ]
+          },
+            {
+                numb: 4,
+            question: "What is the largest rainforest in sri lanka? ",
+            answer: "Sinharaja",
+            options: [
+              "Kanneliya",
+              "Wilpattu",
+              "Sinharaja",
+              "Kumana"
+            ]
+            },
+            {
+                numb: 5,
+            question: "What is the best time to visit Yala? ",
+            answer: "February to June",
+            options: [
+              "January to April",
+              "Whole year",
+              "February to June",
+              "Only February"
+            ]
+            
+            },
+            {
+                numb: 6,
+                question: "What is not an attractive location in Ella rock? ",
+                answer: "World's End",
+                options: [
+                  "Bambaragala Peak",
+                  "Nine Arches Bridge",
+                  "World's End",
+                  "Ravana Ella Falls"
+                ]
+            },
+            {
+                numb: 7,
+                question: "Which district is Ritigala?",
+                answer: "Rathnapura District",
+                options: [
+                  "Galle District",
+                  "Nuwara-Eliya District",
+                  "Colombo District",
+                  "Rathnapura District"
+                ]
+            },
+            {
+                numb: 8,
+                question: "Meemure is,",
+                answer: "A village",
+                options: [
+                  "A forest",
+                  "A village",
+                  "A mountain",
+                  "An island"
+                ]
+            }
+            
+
+        
+
+    ];
+   
